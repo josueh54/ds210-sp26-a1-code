b169aa13df3d47fc7d1833629a408ee5962cab6b
Josue <herediajosue54@gmail.com>
2026-02-16 23:02:10 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -5,8 +5,21 @@ pub struct Part2 {}
 
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
-    fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+    fn guess_the_number(player: &mut Player, mut min: u32, mut max: u32) -> u32 {
+        while min < max {
+            let mid = (min + max) / 2;
+            let result = player.ask_to_compare(mid);
+
+            if result == 0 {
+                return mid;
+
+            } else if result == -1 {
+                max = mid;
+                
+            } else {
+                min = mid + 1;
+            }
+        }
+        min
     }
 }
