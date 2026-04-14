1e87dfbea42eb8a230ecc44ace89ad2af2d39838
Josue <herediajosue54@gmail.com>
2026-04-13 22:37:50 -0400

    Josue implementation

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -13,6 +13,38 @@ impl Agent for SolutionAgent {
     fn solve(board: &mut Board, player: Player) -> (i32, usize, usize) {
         // If you want to make a recursive call to this solution, use
         // `SolutionAgent::solve(...)`
-        unimplemented!("Not yet implemented")
+        if board.game_over() {
+            return (board.score(), 0, 0);
+        }
+
+        let moves = board.moves();
+        let mut best_score = match player {
+            Player::X => i32::MIN,
+            Player::O => i32::MAX,
+        };
+        
+        let mut best_move = moves[0];
+        for m in moves {
+            let mut new_board = board.clone();
+            new_board.apply_move(m, player);
+            
+            let (score, _, _) = SolutionAgent::solve(&mut new_board, player.flip());
+
+            match player {
+                Player::X => {
+                    if score > best_score {
+                        best_score = score;
+                        best_move = m;
+                    }
+                }
+                Player::O => {
+                    if score < best_score {
+                        best_score = score;
+                        best_move = m;
+                    }
+                }
+            }
+        }
+        (best_score, best_move.0, best_move.1)
     }
 }
