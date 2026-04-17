aa8c52a82fd0daf5846280379ef40df5aba23e22
Kinan Dak Albab <kinan.bab.14@gmail.com>
2026-04-17 15:32:06 -0400

    Add module 13

--- module_13_managed_memory/3-cycle.py
+++ module_13_managed_memory/3-cycle.py
@@ -7,28 +7,10 @@ import gc
 x = [1, 2, 3]
 x.append(x)
 x.append(x)
+print(x)
 
-
-
-
-
-print(address(x))
 print(sys.getrefcount(x))
+del x
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#del x
-
-#garbage_items = gc.collect()
-#print(garbage_items)
+count = gc.collect()
+print(count)
--- module_13_managed_memory/4-list.py
+++ module_13_managed_memory/4-list.py
@@ -1,27 +1,16 @@
 from helper import address
 
 
-v = [[1, 2], [3, 4]]
+v = ["hello", "goodbye"]
 x = v[0]
 
 print(address(v[0]))
 print(address(x))
 
+for i in range(10):
+    v.append("this is a long string")
 
+print('is x dangling?', x)
 
-
-
-
-x.append(50)
-print(v)
-
-
-
-for i in range(5):
-    v.append(i)
-
-print(v)
-
-print(x)
 print(address(v[0]))
 print(address(x))
